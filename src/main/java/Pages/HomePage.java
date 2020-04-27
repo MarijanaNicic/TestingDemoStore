d41b7cad1e55f91e26df2c46218bcb05711ae9e5
MarijanaNicic <MarijanaNicic@users.noreply.github.com>
2020-04-27 11:59:47 +0200

    Project commit

---
+++ src/main/java/Pages/HomePage.java
@@ -0,0 +1,12 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    private final WebDriver driver;
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+    }
+}
